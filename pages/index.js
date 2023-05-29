959a32066e3ebb4f0a4d704efc964c0dcf9ec970
Luis Wagner <luiwadev@proton.me>
2023-05-29 03:13:11 +0200

    landing page edited

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+export default function landingPage() {
+    return(
+        <div className="landingPage">
+            <div className="landingPage_title_image_container">
+                <div className="landingPage_title_container">
+                    <h1 className="landingPage_title">uKnight</h1>
+                    <h2 className="landingPage_subtitle">the Knight u want on your Server</h2>
+                </div>
+                <img className="landingPage_knight_image" src="https://img.freepik.com/vektoren-kostenlos/klassischer-spartanischer-helm-mit-farbverlauf_23-2147967737.jpg" alt="knight"></img>
+            </div>
+        </div>
+        
+    )
+}
